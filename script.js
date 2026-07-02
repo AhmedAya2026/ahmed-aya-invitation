279cf22f58d5a80b04dc61c3dd2c2bf539d59257
AhmedAya2026 <ag2769161@gmail.com>
2026-07-02 00:56:33 -0700

    Add wedding invitation alert on button click

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.querySelector(".open-btn");
+
+    btn.addEventListener("click", () => {
+
+        alert("Welcome to Ahmed & Aya's Wedding Invitation 💍");
+
+    });
+
+});
